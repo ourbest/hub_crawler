9b9ec420f60066d467ec8f52e8b5446a1bc80bdf
ourbest <chenyonghui@gmail.com>
2017-07-27 17:48:22 +0800

    remove pid first

--- entry.sh
+++ entry.sh
@@ -1,7 +1,9 @@
 #!/usr/bin/env bash
 
+rm -f celerybeat.pid
+
 celery -A spider beat -l info -S django &
 celery -A spider --concurrency=1 -n worker@%h worker &
 
-gunicorn -w 2 -b 0.0.0.0:8000 --access-logfile logs/access.log spider.wsgi:application
+gunicorn -k gevent -b 0.0.0.0:8000 --access-logfile logs/access.log spider.wsgi:application
 
