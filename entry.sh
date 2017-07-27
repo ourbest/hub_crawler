0fc06071cc92dafd0b3818dcd196d106e9ca9c64
ourbest <chenyonghui@gmail.com>
2017-07-27 18:22:58 +0800

    start nginx

--- entry.sh
+++ entry.sh
@@ -9,5 +9,7 @@ python manage.py migrate
 celery -A spider beat -l info -S django &
 celery -A spider --concurrency=1 -n worker@%h worker &
 
+nginx
+
 gunicorn -k gevent -b 0.0.0.0:8000 --access-logfile /dev/null spider.wsgi:application
 
