20c410c1cd98ec2c90f079cf38b0334499d9ae47
ourbest <chenyonghui@gmail.com>
2017-07-28 11:29:41 +0800

    add entry link

--- spider_app/views.py
+++ spider_app/views.py
@@ -6,14 +6,20 @@ from spider_app.models import Item, Project
 
 def pages(request):
     project_name = request.GET.get('project')
+    entry_id = request.GET.get('entry')
 
     if not project_name:
         project = Project.objects.first()
     else:
         project = Project.objects.filter(name=project_name).first()
 
+    if entry_id:
+        items = Item.objects.filter(entry_id=entry_id).order_by("-pk")[0:100]
+    else:
+        items = Item.objects.filter(entry__project_id=project.id).order_by("-pk")[0:100] if project else []
+
     return render(request, "index.html", context={
         'project': project,
         'projects': Project.objects.all(),
-        'items': Item.objects.filter(entry__project_id=project.id).order_by("-pk")[0:100] if project else []
+        'items': items
     })
