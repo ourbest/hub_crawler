ae8ffd4666d41174756697d7fe17d2c1d33062f6
ourbest <chenyonghui@gmail.com>
2017-07-28 11:48:48 +0800

    use thread

--- spider/celery.py
+++ spider/celery.py
@@ -1,4 +1,6 @@
 import os
+import threading
+from time import sleep
 
 from celery import Celery
 
@@ -10,3 +12,16 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
 app.autodiscover_tasks()
+
+
+class HubThread(threading.Thread):
+    def __init__(self):
+        super(HubThread, self).__init__(name="watch")
+
+    def run(self):
+        while True:
+            sleep(60)
+            from spider_app import tasks
+            tasks.scan_hub()
+
+HubThread().start()
